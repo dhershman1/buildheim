4b7dae3f91c508c886dd670ea093fb292263e400
dhershman1 <dustinh17@gmail.com>
2022-04-26 18:18:59 -0400

    Updated: Routing

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,17 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
+  }, {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }, {
+    path: '/upload',
+    name: 'upload',
+    component: () => import(/* webpackChunkName: "upload" */ '../views/UploadView.vue')
   }
 ]
 
