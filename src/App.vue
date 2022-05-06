005c88c9eb4a6f029810185ff2d9a5f16c491cca
dhershman1 <dustinh17@gmail.com>
2022-05-06 18:39:49 -0400

    Removed: Ant Design

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,24 @@
 <template>
-  <a-layout>
-    <a-layout>
-      <a-layout-header
-        class="layout"
-        theme="dark"
-      >
-        <div class="logo">
-          <router-link
-            v-slot="{ navigate }"
-            to="/"
-          >
-            <a-typography-title
-              :level="3"
-              @click="navigate"
-            >
-              Buildheim
-            </a-typography-title>
-          </router-link>
-        </div>
-        <a-menu
-          v-model:selectedKeys="selectedKeys"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-        >
-          <router-link
-            v-slot="{ navigate }"
-            to="/"
-          >
-            <a-menu-item
-              key="1"
-              @click="navigate"
-            >
-              Home
-            </a-menu-item>
-          </router-link>
-          <router-link
-            v-slot="{ navigate }"
-            to="/about"
-          >
-            <a-menu-item
-              key="2"
-              @click="navigate"
-            >
-              About
-            </a-menu-item>
-          </router-link>
-          <router-link
-            v-slot="{ navigate }"
-            to="/upload"
-          >
-            <a-menu-item
-              key="3"
-              @click="navigate"
-            >
-              Upload
-            </a-menu-item>
-          </router-link>
-        </a-menu>
-      </a-layout-header>
-      <a-layout-content>
-        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">
-          <router-view />
-        </div>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        Buildheim ©2022 Created by <a
-          href="https://github.com/dhershman1"
-          target="_blank"
-        >Dustin Hershman</a>
-      </a-layout-footer>
-    </a-layout>
-  </a-layout>
+  <section>
+    <router-link to="/">
+      Home
+    </router-link>
+    <router-link to="/about">
+      About
+    </router-link>
+    <router-link to="/upload">
+      Upload
+    </router-link>
+    <router-link to="/profile">
+      Profile
+    </router-link>
+    <router-view />
+  </section>
 </template>
 <script>
-import { defineComponent, ref } from 'vue'
-export default defineComponent({
-  setup () {
-    return {
-      selectedKeys: ref(['1'])
-    }
-  }
-})
+export default {
+
+}
 </script>
 
 <style lang="scss" scoped>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import { createApp } from 'vue'
-import Antd from 'ant-design-vue'
 import App from './App'
 import router from './router'
 import store from './store'
 
-import 'ant-design-vue/dist/antd.css'
 import '@/assets/scss/main.scss'
 
 createApp(App)
-  .use(Antd)
   .use(store)
   .use(router)
   .mount('#app')
