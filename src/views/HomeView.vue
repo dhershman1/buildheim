07abc13dc2ca74b8792b0adcf29b30f5b18fae02
dhershman1 <dustinh17@gmail.com>
2022-05-06 20:57:23 -0400

    Removed more Ant design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,35 @@
       src="../assets/logo.png"
     >
     <p>Home View</p>
+    <preview-card>
+      <template #preview-content>
+        <h1>Tavern Build</h1>
+        <h2>It's a Tavern! Whoa!</h2>
+        <p>Something something, booze and unlimited power!</p>
+      </template>
+    </preview-card>
+    <basic-card
+      title="hello"
+      size="sm"
+    >
+      <template #content>
+        <p>Content</p>
+      </template>
+    </basic-card>
   </div>
 </template>
 
 <script>
+import BasicCard from '@/components/BasicCard'
+import PreviewCard from '@/components/PreviewCard'
+
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    BasicCard,
+    PreviewCard
+  }
 }
 </script>
+
+    PreviewCard
